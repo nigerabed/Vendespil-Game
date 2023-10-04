4f225e52a6c9c1dd364c81a91e5dcd7102f84cff
Niger <nigerabed@gmail.com>
2023-10-04 10:25:57 +0200

    adding css

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 import data from "./data.json" assert { type: "json" };
 
-let newdata1 = data.slice(0, 10);
-let newdata2 = data.slice(0, 10);
+// let newdata1 = data.slice(0, 10);
+// let newdata2 = data.slice(0, 10);
 
-function shuffle(arrayData) {
-  for (let i = 0; i < arrayData.length; i++) {
-    let randomIndex = Math.floor(Math.random() * arrayData.length); //Math.floor(Math.random()* max); random er formula.
-    let temp = arrayData[randomIndex];
-    arrayData[randomIndex] = arrayData[i];
-    arrayData[i] = temp;
-  }
-}
+// function shuffle(arrayData) {
+//   for (let i = 0; i < arrayData.length; i++) {
+//     let randomIndex = Math.floor(Math.random() * arrayData.length); //Math.floor(Math.random()* max); random er formula.
+//     let temp = arrayData[randomIndex];
+//     arrayData[randomIndex] = arrayData[i];
+//     arrayData[i] = temp;
+//   }
+// }
 
-shuffle(newdata1);
-shuffle(newdata2);
+// shuffle(newdata1);
+// shuffle(newdata2);
 
 function bigBox(card) {
   let box = document.getElementById("outside-box");
@@ -26,18 +26,22 @@ function bigBox(card) {
   mainDiv.setAttribute("class", "cards");
   container.appendChild(mainDiv);
   mainDiv.innerHTML = `
-<div class="front">
-  <img src="./game.jpg" alt="">
+<div class="front"><img src="./game.jpg" alt="">
 </div>
 
 <div class="back">
- <h3>${card.tag}</h3>
-       
+    <h3>${card.tag}</h3>
 </div>
 `;
 }
 
-for (let i = 0; i < newdata1.length; i++) {
-  bigBox(newdata1[i]);
-  bigBox(newdata2[i]);
-}
+bigBox(data);
+// for (let i = 0; i < newdata1.length; i++) {
+//   bigBox(newdata1[i]);
+//   bigBox(newdata2[i]);
+// }
+
+
+
+//  <h3>${card.tag}</h3>;
+
