3ce12a6b64b0d77315a9333b30709be60df5add2
Niger <nigerabed@gmail.com>
2023-10-12 14:13:02 +0200

    added point

--- script2.js
+++ script2.js
@@ -1,8 +1,14 @@
 import data from "./data.json" assert { type: "json" };
 
-function getASetOfCard(mode) {
-  let cardSet;
+let cardSet;
+let cardSet1;
+let cardSet2;
+let temp1 = "";
+let temp2 = "";
 
+
+
+function getASetOfCard(mode) {
   if (mode === "easy") {
     cardSet = data.slice(0, 3);
   } else if (mode === "medium") {
@@ -14,6 +20,51 @@ function getASetOfCard(mode) {
   return cardSet;
 }
 
+let pointCount = 0;
+let pointElement = document.getElementById("score");
+
+function updatePoint() {
+  pointCount++;
+  pointElement.textContent = pointCount;
+}
+
+function gameOver() {
+  if (pointCount === cardSet.length) {
+    document.getElementById("result").innerText = "Game Over";
+  }
+}
+
+function reset(){
+  document.getElementById("outside-box").innerHTML = "";
+  document.getElementById("result").innerHTML = ""
+  document.getElementById("score").innerHTML = ""
+  pointCount = 0
+   
+}
+
+// SET TIMER 
+
+let minutesLabel = document.getElementById("minutes");
+let secondsLabel = document.getElementById("seconds");
+let totalSeconds = 0;
+setInterval(setTime, 1000);
+
+function setTime() {
+  ++totalSeconds;
+  secondsLabel.innerHTML = pad(totalSeconds % 60);
+  minutesLabel.innerHTML = pad(parseInt(totalSeconds / 60));
+}
+
+function pad(val) {
+  let valString = val + "";
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
+    return valString;
+  }
+}
+
+// Shuffle ARRAY
 function shuffle(arrayData) {
   for (let i = 0; i < arrayData.length; i++) {
     let randomIndex = Math.floor(Math.random() * arrayData.length); //Math.floor(Math.random()* max); random er formula.
@@ -23,60 +74,47 @@ function shuffle(arrayData) {
   }
 }
 
+function displayCardSet(cardSet) {
+  shuffle(cardSet);
+  document.getElementById("outside-box").innerHTML = "";
+  for (let i = 0; i < cardSet.length; i++) {
+    displayCard(cardSet[i]);
+  }
+}
+
 let easyMode = document.getElementById("easyMode");
 easyMode.addEventListener("click", (event) => {
   console.log("Easy Mode");
+  reset()
   let cardSet1 = getASetOfCard("easy"); //data.slice(0, 3);
-  let cardSet2 = getASetOfCard("easy"); //data.slice(0, 3); 
-
-  shuffle(cardSet1);
-  shuffle(cardSet2); 
+  let cardSet2 = getASetOfCard("easy"); //data.slice(0, 3);
+  let cardSet = cardSet1.concat(cardSet2)
+  displayCardSet(cardSet)
   
-  document.getElementById("outside-box").innerHTML = "";
-
-  for(let i = 0; i<cardSet1.length; i++){
-    displayCard(cardSet1[i]);
-    displayCard(cardSet2[i]);
-  }
 });
 
 let mediumMode = document.getElementById("MediumMode");
 mediumMode.addEventListener("click", (event) => {
   console.log("Medium Mode");
+  reset();
   let cardSet1 = getASetOfCard("medium"); //data.slice(0, 7);
   let cardSet2 = getASetOfCard("medium"); //data.slice(0, 7);
-  shuffle(cardSet1);
-  shuffle(cardSet2); 
-
-  document.getElementById("outside-box").innerHTML = "";
-
-  for(let i = 0; i<cardSet1.length; i++){
-    displayCard(cardSet1[i]);
-    displayCard(cardSet2[i]);
-  }
+   let cardSet = cardSet1.concat(cardSet2);
+  displayCardSet(cardSet);
+ 
 });
 
 let hardMode = document.getElementById("HardMode");
 hardMode.addEventListener("click", (event) => {
   console.log("Hard Mode");
-  let cardSet1 = getASetOfCard("hard"); //data.slice(0, 14); 
-  let cardSet2 = getASetOfCard("hard"); //data.slice(0, 14); 
-  shuffle(cardSet1);
-  shuffle(cardSet2); 
-
-  document.getElementById("outside-box").innerHTML = "";
-  for(let i = 0; i<cardSet1.length; i++){
-    displayCard(cardSet1[i]);
-    displayCard(cardSet2[i]);
-  }
+   reset();
+  let cardSet1 = getASetOfCard("hard"); //data.slice(0, 14);
+  let cardSet2 = getASetOfCard("hard"); //data.slice(0, 14);
+   let cardSet = cardSet1.concat(cardSet2);
+   displayCardSet(cardSet);
 });
 
 
-
-
-
-let temp1 = "";
-let temp2 = "";
 
 function displayCard(card) {
   let box = document.getElementById("outside-box");
@@ -111,11 +149,12 @@ function displayCard(card) {
     } else if (temp2 === "") {
       temp2 = mainDiv;
     }
-
     // checking
     if (temp1 !== "" && temp2 !== "") {
       if (temp1.innerHTML === temp2.innerHTML) {
         document.getElementById("result").innerText = "Match";
+        updatePoint();
+        gameOver();
 
         setTimeout(function () {
           temp1.classList.toggle("hidden");
